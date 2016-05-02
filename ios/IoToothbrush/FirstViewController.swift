dd2a458b00b38bf6febfc237151109bbe4a81139
Matthew Lewinski <matthew@lewinski.org>
2016-05-01 21:25:45 -0500

    New iOS Project with Tabbed Application Template

---
+++ ios/IoToothbrush/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IoToothbrush
+//
+//  Created by Matthew Lewinski on 5/1/16.
+//  Copyright © 2016 Matthew Lewinski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/IoToothbrush/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IoToothbrush
+//
+//  Created by Matthew Lewinski on 5/1/16.
+//  Copyright © 2016 Matthew Lewinski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
